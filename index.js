084e348854c4417434b666f5393a63c621b7572c
AbdulAbrahams <abdulabrahams2002@gmail.com>
2023-03-13 11:32:53 +0200

    day 6.8

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "*");
   res.header("Access-Control-Allow-Headers", "*");
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:8080/");
   next();
 });
 
